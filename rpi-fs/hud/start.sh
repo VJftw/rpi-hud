54c84dd0746732dc14fc2be6dc96df7d925c5367
VJ Patel <meetthevj@gmail.com>
2016-06-07 16:54:50 +0100

    Updated to use sudo

--- rpi-fs/hud/start.sh
+++ rpi-fs/hud/start.sh
@@ -3,17 +3,17 @@
 echo ""
 echo "===> Stopping and Removing Docker containers"
 echo ""
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
 echo ""
 echo "===> Cleaning up extra images"
 echo ""
-docker rm -v $(docker ps -a -q -f status=exited)
-docker rmi $(docker images -f "dangling=true" -q)
+sudo docker rm -v $(docker ps -a -q -f status=exited)
+sudo docker rmi $(docker images -f "dangling=true" -q)
 
 ## Pull and Run smart-mirror orchestrate container
-docker pull vjftw/smart-mirror
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v /smart-mirror:/smart-mirror -p 8081:8081 vjftw/smart-mirror
+sudo docker pull vjftw/smart-mirror
+sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v /smart-mirror:/smart-mirror -p 8081:8081 vjftw/smart-mirror
 
 # Wait for ready
 curl -O https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
@@ -24,8 +24,8 @@ echo ""
 ./wait-for-it.sh -t 300 0.0.0.0:8081 -- echo "=> Ochestrate is up"
 
 if [ -f /boot/xinitrc ]; then
-	ln -fs /boot/xinitrc /home/hud_user/.xinitrc;
-	su - hud_user -c 'startx -- -nocursor' &
+	ln -fs /boot/xinitrc /home/hudapp/.xinitrc;
+	startx -- -nocursor &
 fi
 
 exit 0;
