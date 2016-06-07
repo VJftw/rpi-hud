f77cb2b46c2028e3d376f288b45cc0a5670c2401
VJ Patel <meetthevj@gmail.com>
2016-06-07 13:23:34 +0100

    Hopefully fixed install script

--- install.sh
+++ install.sh
@@ -14,27 +14,23 @@ echo FORECASTIO_API_KEY=$forecastio_key > /rpi-hud/environment
 echo ""
 echo "Updating Repositories"
 echo ""
+curl https://raw.githubusercontent.com/VJftw/rpi-hud/develop/rpi-fs/etc/apk/repositories -o /etc/apk/repositories
+chmod 644 /etc/apk/repositories
 apk update
-
-# Install Bash and Font
-echo ""
-echo "Installing Base deps"
-echo ""
-apk add bash curl ttf-freefont
 
 # Install Firefox Browser
 echo ""
 echo "Installing Firfox Browser"
 echo ""
-apk add firefox
+apk add firefox ttf-freefont
 
 # Install X11 and Matchbox
 echo ""
 echo "Installing X11 Server and Openbox Window Manager"
 echo ""
 setup-xorg-base
-​apk add xf86-video-fbdev xf86-input-mouse xf86-input-keyboard dbus
-rc-update ​​add dbus
+​apk add xf86-video-fbdev xf86-input-keyboard dbus
+rc-update add dbus
 
 echo 'Section "Module"
     Load "fbdevhw"
@@ -46,23 +42,20 @@ echo 'Section "Module"
     Disable "dri"
 EndSection' > /etc/X11/xorg.conf.d/20-modules.conf
 
-lbu ci
-
 # Setup Hud User
 echo ""
 echo "Setting up HUD user"
 echo ""
-adduser -h /home/hud_user -D -S
-
-lbu ci
+adduser -h /home/hud_user -D -S hud_user
 
 # Set up Kiosk mode as per http://blogs.wcode.org/2013/09/howto-boot-your-raspberry-pi-into-a-fullscreen-browser-kiosk/
 echo ""
 echo "Installing Kiosk Mode Boot files"
 echo ""
-curl https://raw.githubusercontent.com/VJftw/rpi-hud/develop/rpi-fs/boot/xinitrc -o /boot/xinitrc
+mkdir -p /hud
+curl https://raw.githubusercontent.com/VJftw/rpi-hud/develop/rpi-fs/hud/xinitrc -o /hud/xinitrc
 chmod 755 /boot/xinitrc
-curl https://raw.githubusercontent.com/VJftw/rpi-hud/develop/rpi-fs/boot/start.sh -o /home/hud_user/start.sh
+curl https://raw.githubusercontent.com/VJftw/rpi-hud/develop/rpi-fs/hud/start.sh -o /home/hud_user/start.sh
 chmod 755 /home/hud_user/start.sh
 chown hud_user:hud_user /home/hud_user/start.sh
 
